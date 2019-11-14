4d038a4dcd17e3477780662347c2f7987c9e2fce
Jagadeeshj19 <jagadeesh2994@gmail.com>
2019-11-14 22:09:09 +0530

    message

---
+++ src/sam/Add.java
@@ -0,0 +1,19 @@
+package sam;
+
+public class Add {
+public static void main(String[] args) {
+	
+
+int a=12;
+int b=19;
+int c;
+c=a;
+a=b;
+b=c;
+System.out.println("a value is"+a);
+
+System.out.println("b value is"+b);
+}
+
+
+}
